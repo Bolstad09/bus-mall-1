19a40cc5c96d36b290445a112f506869372ab9f8
Maria Lee-Reed <mariareed@humanoid.net>
2016-12-14 11:13:12 -0800

    Created new function to end survey, added chart

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var imageBox = document.getElementById('imageContainer');
 var startSurvey = document.getElementById('startSurvey');
 
+
 var userClicks = 0;
 var imagesDisplayedOnScreen = 3;
 
 var allProducts = [];
 var displayedProducts = [];
 
+var productNames = [];
+var votes = [];
 
 function Product(name, src, displayed, clicked) {
     this.name = name;
@@ -14,6 +17,7 @@ function Product(name, src, displayed, clicked) {
     this.displayed = displayed;
     this.clicked = clicked;
     allProducts.push(this);
+    productNames.push(this.name);
 }
 
 new Product("bag", './imgdir/bag.jpg', 0, 0);
@@ -27,7 +31,7 @@ new Product("cthulhu", './imgdir/cthulhu.jpg', 0, 0);
 new Product("duckDog", './imgdir/dog-duck.jpg', 0, 0);
 new Product("dragon", './imgdir/dragon.jpg', 0, 0);
 new Product("pen", './imgdir/pen.jpg', 0, 0);
-new Product("pet-sweet", './imgdir/pet-sweep.jpg', 0, 0);
+new Product("pet-sweep", './imgdir/pet-sweep.jpg', 0, 0);
 new Product("scissors", './imgdir/scissors.jpg', 0, 0);
 new Product("shark", './imgdir/shark.jpg', 0, 0);
 new Product("tauntaun", './imgdir/tauntaun.jpg', 0, 0);
@@ -35,6 +39,11 @@ new Product("unicorn", './imgdir/unicorn.jpg', 0, 0);
 new Product("water-can", './imgdir/water-can.jpg', 0, 0);
 new Product("wine-glass", './imgdir/wine-glass.jpg', 0, 0);
 
+
+function handleStart() {
+    createRandomImages();
+    startSurvey.innerHTML = '';
+}
 
 
 function createRandomImages() {
@@ -46,50 +55,42 @@ function createRandomImages() {
     for(var i=0; i < imagesDisplayedOnScreen; i++) {
         var image = document.createElement('img');
         var randomIndex = Math.floor(Math.random() * allProducts.length);
+
         image.src = allProducts[randomIndex].src;
         image.id = allProducts[randomIndex].name;
         imageContainer.appendChild(image);
+
         allProducts[randomIndex].displayed += 1;
+
         displayedProducts.push(allProducts[randomIndex]);
         allProducts.splice(randomIndex, 1);
     }
-    console.log(allProducts);
-    console.log(displayedProducts);
     return;
 }
+
 
 
 function clearImages() {
     imageBox.innerHTML = '';
 }
 
-
-
 function resetArrays() {
     allProducts = allProducts.concat(displayedProducts);
     displayedProducts = [];
 }
 
-function handleStart() {
-    createRandomImages();
-    startSurvey.innerHTML = '';
-}
+
 
 function handleRandomize(event) {
     event.preventDefault();
 
-    if(userClicks === 25) {
-        clearImages();
-        alert("Out of clicks");
-        imageBox.removeEventListener('click', handleRandomize);
-        resetArrays();
+    if(event.target === event.currentTarget) {
+        alert('Please select an image!');
+        return;
+    }
 
-        var resultsButtonHere = document.getElementById('resultsButtonHere')
-        var resultButton = document.createElement('button');
-        resultButton.type = 'submit';
-        resultButton.textContent = 'Show Results';
-        resultsButtonHere.appendChild(resultButton);
-        resultsButtonHere.addEventListener('click', showResults)
+    if(userClicks === 25) {
+        endSurvey();
         return;
     }
 
@@ -104,17 +105,67 @@ function handleRandomize(event) {
     }
 }
 
-function showResults(event){
-    event.preventDefault()
-    var results = document.getElementById('results');
-    resultsButtonHere.innerHTML = '';
+
+
+
+function endSurvey(){
+    clearImages();
+    resetArrays();
+    alert("Out of clicks");
+    imageBox.removeEventListener('click', handleRandomize);
+
+
+    for(var i = 0; i < allProducts.length; i++) {
+        votes[i] = allProducts[i].clicked;
+    }
+    console.log(votes);
+
+    var resultsListButtonHere = document.getElementById('resultsListButtonHere');
+    var resultListButton = document.createElement('button');
+        resultListButton.type = 'submit';
+        resultListButton.textContent = 'Show list Results';
+        resultsListButtonHere.appendChild(resultListButton);
+        resultsListButtonHere.addEventListener('click', showListResults);
+
+    var chartButtonHere = document.getElementById('chartButtonHere');
+    var chartButton = document.createElement('button');
+        chartButton.type = 'submit';
+        chartButton.textContent = 'Show Chart Results';
+        chartButtonHere.appendChild(chartButton);
+        chartButtonHere.addEventListener('click', showChartResults);
+}
+
+
+function showListResults(event){
+    event.preventDefault();
+    resultsListButtonHere.innerHTML = '';
+    resultsListButtonHere.removeEventListener('click', showListResults);
+    var resultsList = document.getElementById('resultsList');
     for(var i=0; i < allProducts.length; i++) {
         var liEl = document.createElement('li');
         liEl.textContent=allProducts[i].name + ' was clicked ' + allProducts[i].clicked + ' times, after being displayed ' + allProducts[i].displayed + ' times.';
-        results.appendChild(liEl);
+        resultsList.appendChild(liEl);
     }
 }
 
+
+var barData = {
+    labels : productNames,
+    datasets : [
+        {
+        data : votes,
+        }
+    ]
+};
+
+function showChartResults(event){
+    event.preventDefault();
+    chartButtonHere.innerHTML = '';
+    chartButtonHere.removeEventListener('click', showChartResults);
+
+    var results = document.getElementById('results').getContext('2d');
+    var resultChart = new Chart(results).Bar(barData);
+}
 
 
 startSurvey.addEventListener('click', handleStart);
